4bcd9e2f54405fa2321b351daf68fd4a028db606
Vladyslav Pustovalov <vladyslav.pustovalov@gmail.com>
2025-05-17 17:03:56 +0200

    Initial Commit

---
+++ the-discipline-program-ios/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  the-discipline-program-ios
+//
+//  Created by Vladyslav Pustovalov on 17/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ the-discipline-program-ios/the_discipline_program_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  the_discipline_program_iosApp.swift
+//  the-discipline-program-ios
+//
+//  Created by Vladyslav Pustovalov on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct the_discipline_program_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
