964e48de58f661edb1a21995f0689497b71b3811
Vladyslav Pustovalov <vladyslav.pustovalov@gmail.com>
2025-06-25 19:36:48 +0200

    extract ViewModel to separate classes

--- the-discipline-program-ios/ViewModels/ProgramViewModel.swift
+++ the-discipline-program-ios/ViewModels/ProgramViewModel.swift
@@ -8,81 +8,79 @@
 import KeychainAccess
 import SwiftUI
 
-extension ProgramView {
+@Observable class ProgramViewModel {
+    let keychain = Keychain(service: Constants.Bundle.id)
     
-    @Observable class ViewModel {
-        let keychain = Keychain(service: Constants.Bundle.id)
-
-        var authToken: String?
-        var userId: Int?
-        var programDate: Date
-        var program: Program?
-        var programError: NetworkResponseStatus?
+    var authToken: String?
+    var userId: Int?
+    var programDate: Date
+    var program: Program?
+    var programError: NetworkResponseStatus?
+    
+    init(programDate: Date) {
+        self.programDate = programDate
         
-        init(programDate: Date) {
-            self.programDate = programDate
-
-            authToken = try? keychain.get(Constants.Bundle.tokenKey)
-            userId = UserDefaults.standard.integer(forKey: Constants.Defaults.userId)
+        authToken = try? keychain.get(Constants.Bundle.tokenKey)
+        userId = UserDefaults.standard.integer(forKey: Constants.Defaults.userId)
+        
+        loadProgram(for: programDate)
+    }
+    
+    func loadProgram(for date: Date) {
+        Task {
             
-            loadProgram(for: programDate)
-        }
-        
-        func loadProgram(for date: Date) {
-            Task {
-                
-                print("Date inside loadProgram: \(date)")
-                
-                do {
-                    guard let userId else {
-                        print("Nil userId in loadProgram")
-                        return
-                    }
-                    guard let authToken else {
-                        print("Nil authToken in loadProgram")
-                        return
-                    }
-                    let result = try await NetworkService.shared.loadProgram(
-                        authToken: authToken,
-                        userId: userId,
-                        date: date
-                    )
-                    
-                    switch result {
-                    case .success(let tempProgram):
-                        print("Program: \(tempProgram.scheduledDate)")
-                        await MainActor.run {
-                            program = tempProgram
-                        }
-                    case .failure(let error):
-                        if error.code == 403 {
-
-                            return
-                        }
-                        if error.code == 404 {
-                            print("Program Not Found")
-                            await MainActor.run {
-                                self.program = nil
-                                self.programError = error
-                            }
-                        }
-                        throw error
-                    }
-                } catch {
-                    print("❌ Program failed: \(error)")
-                    print("Error message: \(error.localizedDescription)")
+            print("Date inside loadProgram: \(date)")
+            
+            do {
+                guard let userId else {
+                    print("Nil userId in loadProgram")
+                    return
                 }
+                guard let authToken else {
+                    print("Nil authToken in loadProgram")
+                    return
+                }
+                let result = try await NetworkService.shared.loadProgram(
+                    authToken: authToken,
+                    userId: userId,
+                    date: date
+                )
+                
+                switch result {
+                case .success(let tempProgram):
+                    print("Program: \(tempProgram.scheduledDate)")
+                    await MainActor.run {
+                        program = tempProgram
+                    }
+                case .failure(let error):
+                    if error.code == 403 {
+                        
+                        return
+                    }
+                    if error.code == 404 {
+                        print("Program Not Found")
+                        await MainActor.run {
+                            self.program = nil
+                            self.programError = error
+                        }
+                    }
+                    throw error
+                }
+            } catch {
+                print("❌ Program failed: \(error)")
+                print("Error message: \(error.localizedDescription)")
             }
         }
-        
-        func loadNextDay() {
-            programDate = Calendar.current.date(byAdding: .day, value: 1, to: programDate)!
-            loadProgram(for: programDate)
-        }
-
-        func loadPreviousDay() {
-            programDate = Calendar.current.date(byAdding: .day, value: -1, to: programDate)!
-            loadProgram(for: programDate)
-        }
+    }
+    
+    func loadNextDay() {
+        programDate = Calendar.current.date(byAdding: .day, value: 1, to: programDate)!
+        loadProgram(for: programDate)
+    }
+    
+    func loadPreviousDay() {
+        programDate = Calendar.current.date(byAdding: .day, value: -1, to: programDate)!
+        loadProgram(for: programDate)
     }
 }
+
