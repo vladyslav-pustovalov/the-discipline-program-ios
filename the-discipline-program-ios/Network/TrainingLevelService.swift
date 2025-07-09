c063d44964f67c1c62821ef33fea31fecabc8b5a
Vladyslav Pustovalov <vladyslav.pustovalov@gmail.com>
2025-07-09 22:10:55 +0200

    add TrainingLevelService

--- the-discipline-program-ios/Network/TrainingLevelService.swift
+++ the-discipline-program-ios/Network/TrainingLevelService.swift
@@ -6,3 +6,29 @@
 //
 
 import Foundation
+
+final class TrainingLevelService: NetworkService {
+    
+    func loadTrainingLevels(authToken: String) async throws -> Result<[TrainingLevel], NetworkResponseStatus> {
+        let headers = [
+            "Content-Type": "application/json",
+            "Authorization": authToken
+        ]
+        
+        let result = try await performRequest(
+            stringURL: "\(baseURL)/trainingLevel/all",
+            method: Constants.HTTPMethods.get,
+            headers: headers,
+            body: nil
+        )
+        
+        switch result {
+        case .success(let data):
+            let trainingLevels = try BaseDecoder().decode([TrainingLevel].self, from: data)
+            print("Level: \(trainingLevels.first?.name)")
+            return .success(trainingLevels)
+        case .failure(let status):
+            return .failure(status)
+        }
+    }
+}
