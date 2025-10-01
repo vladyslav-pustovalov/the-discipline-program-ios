4ba03477ed33bb5d986ac8c31be1aa05e67df0e0
vladyslav.pustovalov <vladyslav.pustovalov@gmail.com>
2025-10-01 11:21:11 +0200

    add UserPlan entity

--- TheDisciplineProgram/Models/UserPlan.swift
+++ TheDisciplineProgram/Models/UserPlan.swift
@@ -6,3 +6,18 @@
 //
 
 import Foundation
+
+struct UserPlan: Codable, Identifiable, Hashable, Equatable {
+    private(set) var id: Int
+    private(set) var name: String
+}
+
+enum UserPlanType {
+    case general
+    case individual
+}
+
+extension UserPlan {
+    static var general = UserPlan(id: 1, name: "General")
+    static var individual = UserPlan(id: 2, name: "Individual")
+}
